14bb8689f9878f291c0f42271ac3772943022896
rpire <rubenpire7@gmail.com>
2021-08-09 10:18:32 -0400

    Apply fix for ESLint erros

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 let books = [
-	{
-		title: 'Lorem ipsum dolor',
-		author: 'Lorem ipsum',
-	},
-	{
-		title: 'Another book',
-		author: 'Another Author',
-	},
+  {
+    title: 'Lorem ipsum dolor',
+    author: 'Lorem ipsum',
+  },
+  {
+    title: 'Another book',
+    author: 'Another Author',
+  },
 ];
 
 function genHTML() {
-	const htmlString = `
+  const htmlString = `
     <article class="book-card">
         <p class="book-title">Lorem ipsum dolor sit amet.</p>
         <p class="book-author">Lorem, ipsum.</p>
@@ -18,48 +18,48 @@ function genHTML() {
         <hr>
     </article>
     `;
-	for (let i = 0; i < books.length; i += 1) {
-		document.getElementById('book-shelf').innerHTML += htmlString;
-		const title = document.querySelectorAll('.book-title');
-		const author = document.querySelectorAll('.book-author');
-		const button = document.querySelectorAll('.book-btn');
-		title[i].innerHTML = books[i].title;
-		author[i].innerHTML = books[i].author;
-		button[i].setAttribute('onclick', `removeBook(${i})`);
-	}
+  for (let i = 0; i < books.length; i += 1) {
+    document.getElementById('book-shelf').innerHTML += htmlString;
+    const title = document.querySelectorAll('.book-title');
+    const author = document.querySelectorAll('.book-author');
+    const button = document.querySelectorAll('.book-btn');
+    title[i].innerHTML = books[i].title;
+    author[i].innerHTML = books[i].author;
+    button[i].setAttribute('onclick', `removeBook(${i})`);
+  }
 }
 
 window.onload = () => {
-	if (localStorage.getItem('Books')) {
-		const oldStorage = localStorage.getItem('Books');
-		const newStorage = JSON.parse(oldStorage);
-		books = newStorage;
-		genHTML();
-	} else {
-		genHTML();
-	}
+  if (localStorage.getItem('Books')) {
+    const oldStorage = localStorage.getItem('Books');
+    const newStorage = JSON.parse(oldStorage);
+    books = newStorage;
+    genHTML();
+  } else {
+    genHTML();
+  }
 };
 
 function addBook() {
-	const title = document.querySelector('#title').value;
-	const author = document.querySelector('#author').value;
+  const title = document.querySelector('#title').value;
+  const author = document.querySelector('#author').value;
 
-	const bookData = {
-		title,
-		author,
-	};
+  const bookData = {
+    title,
+    author,
+  };
 
-	books.push(bookData);
+  books.push(bookData);
 
-	let bookList = JSON.stringify(books);
-	localStorage.setItem('Books', bookList);
+  const bookList = JSON.stringify(books);
+  localStorage.setItem('Books', bookList);
 }
 
-removeBook = (num) => {
-	books.splice(num, 1);
-	const bookList = JSON.stringify(books);
-	localStorage.setItem('Books', bookList);
-	window.location.reload();
+const removeBook = (num) => {
+  books.splice(num, 1);
+  const bookList = JSON.stringify(books);
+  localStorage.setItem('Books', bookList);
+  window.location.reload();
 }
 
 const submitButton = document.querySelector('#button');
