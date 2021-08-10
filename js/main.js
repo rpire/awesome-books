cd402e7e5e5e6912ae7dd4c1786c4ce0e2f5d630
Matongo Mulindi <mmatongo_@hotmail.com>
2021-08-10 11:39:34 +0200

    finalize main.js logic & functionality

--- js/main.js
+++ js/main.js
@@ -13,6 +13,7 @@ class BookShelf {
       },
     ];
   }
+
   genHTML() {
     const htmlString = `
       <article class="book-card">
@@ -29,9 +30,10 @@ class BookShelf {
       const button = document.querySelectorAll('.book-btn');
       title[i].innerHTML = this.books[i].title;
       author[i].innerHTML = this.books[i].author;
-      button[i].setAttribute('onclick', `removeBook(${i})`);
+      button[i].setAttribute('onclick', `bookShelf.removeBook(${i})`);
     }
   }
+
   addBook(title, author) {
     const bookData = {
       title,
@@ -40,8 +42,8 @@ class BookShelf {
     this.books.push(bookData);
     const bookList = JSON.stringify(this.books);
     localStorage.setItem('Books', bookList);
-    console.log('hello world')
   }
+
   reload() {
     if (localStorage.getItem('Books')) {
       const oldStorage = localStorage.getItem('Books');
@@ -52,77 +54,19 @@ class BookShelf {
       this.genHTML();
     }
   }
+
+  removeBook(num) {
+    this.books.splice(num, 1);
+    const bookList = JSON.stringify(this.books);
+    localStorage.setItem('Books', bookList);
+    window.location.reload();
+  }
 }
 
-const bookShelf = new BookShelf;
+const bookShelf = new BookShelf();
 window.onload = bookShelf.reload();
-
-// let books = [
-//   {
-//     title: 'Lorem ipsum dolor',
-//     author: 'Lorem ipsum',
-//   },
-//   {
-//     title: 'Another book',
-//     author: 'Another Author',
-//   },
-// ];
-
-// const genHTML = () => {
-//   const htmlString = `
-//     <article class="book-card">
-//         <p class="book-title"></p>
-//         <p class="book-author"></p>
-//         <button class="book-btn">Remove</button>
-//         <hr>
-//     </article>
-//     `;
-//   for (let i = 0; i < bookShelf.books.length; i += 1) {
-//     document.getElementById('book-shelf').innerHTML += htmlString;
-//     const title = document.querySelectorAll('.book-title');
-//     const author = document.querySelectorAll('.book-author');
-//     const button = document.querySelectorAll('.book-btn');
-//     title[i].innerHTML = bookShelf.books[i].title;
-//     author[i].innerHTML = bookShelf.books[i].author;
-//     button[i].setAttribute('onclick', `removeBook(${i})`);
-//   }
-// };
-
-// window.onload = () => {
-//   if (localStorage.getItem('Books')) {
-//     const oldStorage = localStorage.getItem('Books');
-//     const newStorage = JSON.parse(oldStorage);
-//     bookShelf.books = newStorage;
-//     genHTML();
-//   } else {
-//     genHTML();
-//   }
-// };
-
-// const addBook = () => {
-//   const title = document.querySelector('#title').value;
-//   const author = document.querySelector('#author').value;
-
-//   const bookData = {
-//     title,
-//     author,
-//   };
-
-//   books.push(bookData);
-
-//   const bookList = JSON.stringify(books);
-//   localStorage.setItem('Books', bookList);
-// };
-
-const removeBook = (num) => {
-  books.splice(num, 1);
-  const bookList = JSON.stringify(books);
-  localStorage.setItem('Books', bookList);
-  window.location.reload();
-};
 
 const title = document.querySelector('#title');
 const author = document.querySelector('#author');
-console.log(title, author);
 const submitButton = document.querySelector('#button');
-submitButton.addEventListener('click', bookShelf.addBook(title, author));
+submitButton.onclick = () => { bookShelf.addBook(title.value, author.value); };
